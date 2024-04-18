b37e17900eac4755119c70f78b780bd0285ac178
Macelia <massiliasaidoune06@gmail.com>
2024-04-19 00:22:44 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Link, Outlet, Route, RouterProvider, createBrowserRouter, createRoutesFromElements, useLoaderData } from 'react-router-dom';
 import './App.css';
 import Acceuil from './componenets/Acceuil'
-import UserProfile from './componenets/UserProfile'
+import UserProfile, { photosLoader } from './componenets/UserProfile'
 import Contact from './componenets/UserProfile'
 
 
@@ -11,7 +11,7 @@ function App() {
    <Route path='/' element={<Root/>}>
 
    <Route  index  element={<Acceuil/>}/>
-   <Route path='/userprofile' element={<UserProfile/>}/>
+   <Route path='/userprofile' element={<UserProfile/>} loader={photosLoader}/>
    <Route path='/contact' element={<Contact/>}/>
 
    </Route>
@@ -31,6 +31,7 @@ export default App;
 
 const Root =()=>{
   return <>
+  <div className='BlurBackground'>
   <div className='Link'>
     <Link to='/'>Acceuil</Link>
     <Link to='/userprofile'>User-Profile</Link>
@@ -41,5 +42,6 @@ const Root =()=>{
   <div className="Content">
         <Outlet></Outlet>
       </div>
+    </div>
   </>
 }
--- src/componenets/UserProfile.js
+++ src/componenets/UserProfile.js
@@ -1,11 +1,49 @@
+// Sur la page de profil utilisateur, affichez des informations de profil factices (par exemple, nom, email, etc.).
 import React from 'react'
+import { useLoaderData } from 'react-router-dom';
 
 const UserProfile = () => {
+
+  const users=useLoaderData();
+
   return (
     <div>
+      <h1>Our User's Data :</h1>
+     
+     <ul>
+     { users.map((user,index)=>(
+      <li key={index}>
+
+   
+        <img src={user.photo} alt="User"/>
       
+      <div>
+        <p>Name: {user.name}</p>
+        <p>Email:{user.email}</p>
+      </div>
+      </li>
+        ))}
+      </ul>
     </div>
   )
 }
 
+
+export const  photosLoader=async()=>{
+try{
+  const response =await fetch('https://randomuser.me/api/?results=5');
+  const data = await response.json();
+  const users = data.results.map(user =>({
+    name:`${user.name.first}${user.name.last}`,
+    email:user.email,
+    photo:user.picture.large,
+  }));
+  return users;
+}
+catch(error){console.error('error fetching user data:',error);
+return [];
+}
+ }
 export default UserProfile
+
+
